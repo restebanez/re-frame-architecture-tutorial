ef66084322f80e2f33be454e221bfb0c84a836d3
Rodrigo Estebanez <rodhom@gmail.com>
2016-12-05 13:08:49 +0000

    change var names as in the elm version

--- src/examples/d_http.cljs
+++ src/examples/d_http.cljs
@@ -7,12 +7,12 @@
             [re-frisk.core :refer [enable-re-frisk!]]))
 
 
-
 (re-frame/reg-event-db
  :initialize-db
  (fn  [_ _]
-   {:loading? false
-    :image_url ""
+   {:topic "waiting.gif"
+    :gif-url ""
+    :loading? false
     :response-error ""}))
 
 
@@ -20,29 +20,31 @@
 
 
 (re-frame/reg-event-fx                             ;; note the trailing -fx
-    :request-more-cats                      ;; usage:  (dispatch [:handler-with-http])
-       (fn [{:keys [db]} _]                    ;; the first param will be "world"
-         {:db   (assoc db :loading? true)   ;; causes the twirly-waiting-dialog to show??
-          :http-xhrio {:method          :get
-                       :uri             "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=cats"
-                       :timeout         8000                                           ;; optional see API docs
-                       :response-format (ajax/json-response-format {:keywords? true})  ;; IMPORTANT!: You must provide this.
-                       :on-success      [:response-more-cats-sucess]
-                       :on-failure      [:response-more-cats-failure]}}))
+  :more-please                      ;; usage:  (dispatch [:handler-with-http])
+  (fn [{:keys [db]} [_ topic]]                ;; the first param will be "world"
+      {:db   (-> db
+                (assoc :topic topic)
+                (assoc :loading? true))   ;; causes the twirly-waiting-dialog to show??
+       :http-xhrio {:method          :get
+                    :uri             (str "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=" topic)
+                    :timeout         8000                                           ;; optional see API docs
+                    :response-format (ajax/json-response-format {:keywords? true})  ;; IMPORTANT!: You must provide this.
+                    :on-success      [:response-more-please-success]
+                    :on-failure      [:response-more-please-failure]}}))
 
 
 (re-frame/reg-event-db
-  :response-more-cats-sucess
+ :response-more-please-success
   (fn
     [db [_ response]]           ;; destructure the response from the event vector
     (-> db
         (assoc :loading? false) ;; take away that "Loading ..." UI
         (assoc :response-error "") ;; clear error
-        (assoc :image_url (get-in (js->clj response) [:data :image_url])))))
+        (assoc :gif-url (get-in (js->clj response) [:data :image_url])))))
 
 
 (re-frame/reg-event-db
-  :response-more-cats-failure
+ :response-more-please-failure
   (fn
     [db [_ response]]           ;; destructure the response from the event vector
     (-> db
@@ -51,24 +53,31 @@
 
 ;; -- Subscription Handlers ---------------------------------------------------
 
+(re-frame/reg-sub
+ :topic
+ (fn [db]
+   (:topic db)))
 
 (re-frame/reg-sub
- :image_url
+ :gif-url
  (fn [db]
-   (:image_url db)))
+   (:gif-url db)))
 
 
 ;; -- View Components ---------------------------------------------------------
 
 
 (defn main-panel []
-  (let [image-url (re-frame/subscribe [:image_url])]
+  (let [image-url (re-frame/subscribe [:gif-url])
+        topic     (re-frame/subscribe [:topic])]
+
     (fn []
       [:div
-       [:h1 "Catss"]
-       [:input {:type "button" :value "More cats!"
-                :on-click #(re-frame/dispatch [:request-more-cats])}]
-       [:div "This is the picture " [:img {:src @image-url}]]])))
+       [:h2 @topic]
+       [:input {:type "button" :value "More Please!!"
+                :on-click #(re-frame/dispatch [:more-please "cats"])}]
+       [:br]
+       [:img {:src @image-url}]])))
 
 (defn mount-root []
   (reagent/render [main-panel]
