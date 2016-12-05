3281fc039b8a4fb9abc5db054e4990d1b6a2edd1
Rodrigo Estebanez <rodhom@gmail.com>
2016-12-05 11:54:23 +0000

    initial commit

---
+++ project.clj
@@ -0,0 +1,47 @@
+(defproject cats "0.1.0-SNAPSHOT"
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.9.229"]
+                 [reagent "0.6.0"]
+                 [re-frame "0.8.0"]
+                 [cljs-ajax                  "0.5.8"]
+                 [day8.re-frame/http-fx "0.1.2"]
+                 [re-frisk "0.3.1"]]
+
+  :plugins [[lein-cljsbuild "1.1.4"]]
+
+  :min-lein-version "2.5.3"
+
+  :source-paths ["src/examples"]
+
+  :clean-targets ^{:protect false} ["resources/public/js/compiled" "target"]
+
+  :figwheel {:css-dirs ["resources/public/css"]}
+
+  :profiles
+  {:dev
+   {:dependencies [[binaryage/devtools "0.8.2"]]
+
+    :plugins      [[lein-figwheel "0.5.7"]]}}
+
+
+  :cljsbuild
+  {:builds
+   [{:id           "dev"
+     :source-paths ["src/examples"]
+     :figwheel     {:on-jsload "examples.d-http/mount-root"}
+     :compiler     {:main                 examples.d-http
+                    :output-to            "resources/public/js/compiled/app.js"
+                    :output-dir           "resources/public/js/compiled/out"
+                    :asset-path           "js/compiled/out"
+                    :source-map-timestamp true
+                    :preloads             [devtools.preload]
+                    :external-config      {:devtools/config {:features-to-install :all}}}}
+
+
+    {:id           "min"
+     :source-paths ["src/examples"]
+     :compiler     {:main            examples.d-http
+                    :output-to       "resources/public/js/compiled/app.js"
+                    :optimizations   :advanced
+                    :closure-defines {goog.DEBUG false}
+                    :pretty-print    false}}]})
